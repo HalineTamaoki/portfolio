133d3f477fb6275c96649add909f50aced9af785
Haline Tamaoki <haline.tamaoki@gmail.com>
2025-07-02 21:57:11 -0300

    Implemented react hook form

---
+++ src/__tests__/components/ContactForm.test.tsx
@@ -0,0 +1,65 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { act } from "react";
+import { vi } from "vitest";
+import { ContactForm } from "../../components/portfolio/contact/ContactForm";
+import MockRender from "../__mocks__/MockRender";
+
+vi.mock('@emailjs/browser', () => ({
+  sendForm: vi.fn(() => Promise.resolve({ status: 200, text: 'OK' })),
+}));
+
+const setEmailStatus = vi.fn();
+
+const customRender = () => render(
+    <MockRender>
+        <ContactForm setEmailStatus={setEmailStatus} />
+    </MockRender>
+);
+
+describe("Contact Form tests", () => {
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+
+    test("should render the element correctly", () => {
+        customRender();
+
+        expect(screen.getByTestId('contact-form-email')).toBeInTheDocument();
+        expect(screen.getByTestId('contact-form-name')).toBeInTheDocument();
+        expect(screen.getByTestId('contact-form-message')).toBeInTheDocument();
+        expect(screen.getByTestId('contact-send')).toBeInTheDocument();
+    });
+
+    test("should Validate", async () => {
+        customRender();
+
+        const sendButton = screen.getByTestId('contact-send');
+        act(() => fireEvent.click(sendButton));
+        expect(await screen.findAllByText('form-mandatory')).toHaveLength(3);
+
+        const emailField = screen.getByTestId('contact-form-email');
+        act(() => fireEvent.change(emailField, {target: {value: 'INVALID'}}));
+
+        expect(await screen.findByText('form-email-error')).toBeInTheDocument();
+    });
+
+    test("should send email", async () => {
+        customRender();
+
+        const nameField = screen.getByTestId('contact-form-name');
+        act(() => fireEvent.change(nameField, {target: {value: 'Name'}}));
+
+        const emailField = screen.getByTestId('contact-form-email');
+        act(() => fireEvent.change(emailField, {target: {value: 'email@email.com'}}));
+
+        const messageField = screen.getByTestId('contact-form-message');
+        act(() => fireEvent.change(messageField, {target: {value: 'Message'}}));
+
+        const sendButton = screen.getByTestId('contact-send');
+        act(() => fireEvent.click(sendButton));
+
+        waitFor(() => {
+            expect(setEmailStatus).toHaveBeenCalledWith('success');
+        })
+    });
+});
--- src/components/portfolio/contact/ContactForm.tsx
+++ src/components/portfolio/contact/ContactForm.tsx
@@ -1,21 +1,18 @@
 import { sendForm } from "@emailjs/browser";
 import { Box, Button, CircularProgress, TextField, useTheme } from "@mui/material";
 import { grey } from "@mui/material/colors";
-import { ChangeEvent, Dispatch, FocusEvent, FormEvent, SetStateAction, useCallback, useMemo, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useMemo, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { EMAIL_PUBLIC_KEY, EMAIL_SERVICE_ID, EMAIL_TEMPLATE_ID } from "../../../common/enviroment";
-import { useDebounce } from "../../../hooks/useDebounce";
 
-enum FieldErrors {
-    VALUE_MISSING = 'VALUE_MISSING',
-    TYPE_MISMATCH = 'TYPE_MISMATCH'
-}
-
-export default function ContactForm({setEmailStatus}: {setEmailStatus: Dispatch<SetStateAction<"error" | "success" | undefined>>}) {
+export function ContactForm({setEmailStatus}: {setEmailStatus: Dispatch<SetStateAction<"error" | "success" | undefined>>}) {
     const theme = useTheme();
     const { t } = useTranslation();
-    const form = useRef<HTMLFormElement>(null);
-    const [ errors, setErrors ] = useState<{[key: string]: FieldErrors | undefined}>({});
+    const { handleSubmit, register, reset, formState: { errors } } = useForm({
+        mode: 'all'
+    });
+    const formRef = useRef<HTMLFormElement>(null)
     const [ isSendingEmail, setIsSendingEmail ] = useState<boolean>(false);
 
     const textFieldSlotProps = useMemo(() => ({
@@ -50,37 +47,13 @@ export default function ContactForm({setEmailStatus}: {setEmailStatus: Dispatch<
         }
     }), [theme.palette.mode]);
 
-    const validate = useCallback(({name, validity}: {name: string, validity: ValidityState}) => {
-        if(validity.valueMissing){
-            setErrors((prev) => ({...prev, [name]: FieldErrors.VALUE_MISSING}));
-            return;
-        } 
+    const sendEmail = () => { 
+        if(!formRef.current) return; 
 
-        if(validity.typeMismatch){
-            setErrors((prev) => ({...prev, [name]: FieldErrors.TYPE_MISMATCH}));
-            return;
-        }
-
-        setErrors((prev) => ({...prev, [name]: undefined}));
-    }, [errors])
-
-    const debouncedValidate = useDebounce(validate, 500);
-
-    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | 
-        FocusEvent<HTMLInputElement | HTMLTextAreaElement>
-    ): void => {
-        const { name, validity } = e.target; 
-
-        debouncedValidate({name, validity})    
-    }    
-
-    const sendEmail = () => {
-        if(!form.current) return;
- 
         setIsSendingEmail(true);
-        sendForm(EMAIL_SERVICE_ID, EMAIL_TEMPLATE_ID, form.current, {publicKey: EMAIL_PUBLIC_KEY})
+        sendForm(EMAIL_SERVICE_ID, EMAIL_TEMPLATE_ID, formRef.current, {publicKey: EMAIL_PUBLIC_KEY})
             .then(() => {
-                form.current?.reset()
+                reset()
                 setEmailStatus('success');
             })
             .catch((err) => {
@@ -88,77 +61,51 @@ export default function ContactForm({setEmailStatus}: {setEmailStatus: Dispatch<
                 setEmailStatus('error')
             })
             .finally(() => setIsSendingEmail(false));
-    }
-    
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        if(!form.current) return;
-        const formErrors = Array.from(form.current.elements).reduce((acc, element) => {
-            const input = element as HTMLInputElement;
-            if (!input.name) return acc;
-
-            if (!input.value){
-                acc[input.name] = FieldErrors.VALUE_MISSING;
-            } else if(input.validity.typeMismatch){
-                acc[input.name] = FieldErrors.TYPE_MISMATCH;
-            } else {
-                acc[input.name] = undefined;
-            }
-            return acc;
-        }, errors);
-
-        if (Object.values(formErrors).filter(value => !!value).length > 0){
-            setErrors(formErrors);
-        } else {
-            sendEmail();
-        }
-    }
+    };
     
     return (
-        <Box component='form' ref={form} noValidate id="contact-form" gap={2} display={'grid'} onSubmit={handleSubmit}>
+        <Box component='form' ref={formRef} onSubmit={handleSubmit(sendEmail)} id="contact-form" gap={2} display={'grid'}>
             <TextField
+                {...register('name', {required: t('form-mandatory', {field: t('contact-name')})})}
+                id="contact-form-name"
                 error={!!errors?.name} 
                 fullWidth 
-                name="name"
                 label={t('contact-name')} 
                 variant="filled" 
                 size="small" 
                 slotProps={textFieldSlotProps}
-                onChange={handleChange}
-                onBlur={handleChange}
-                required
-                helperText={errors?.name === FieldErrors.VALUE_MISSING ? t('form-mandatory', {field: t('contact-name')}) : undefined}
+                helperText={typeof errors?.name?.message === 'string' && errors?.name?.message}
             />
             <TextField 
+                {...register('email', {
+                    required: t('form-mandatory', {field: t('contact-email')}),
+                    pattern: {
+                        value: /^\S+@\S+\.\S+$/,
+                        message: t('form-email-error')
+                    }
+                })}
+                id="contact-form-email"
                 error={!!errors?.email} 
                 fullWidth 
-                name="email"
                 label={t('contact-email')} 
                 variant="filled" 
                 size="small" 
                 type='email'
                 slotProps={textFieldSlotProps}
-                onChange={handleChange}
-                onBlur={handleChange}
-                required
-                helperText={errors?.email === FieldErrors.VALUE_MISSING ? t('form-mandatory', {field: t('contact-email')}) : 
-                    (errors?.email === FieldErrors.TYPE_MISMATCH ? t('form-email-error') : undefined)}
+                helperText={typeof errors?.email?.message === 'string' && errors?.email?.message}
             />
             <TextField 
+                {...register('message', {required: t('form-mandatory', {field: t('contact-message')})})}
+                id="contact-form-message"
                 error={!!errors?.message} 
                 fullWidth 
-                name="message"
                 label={t('contact-message')} 
                 variant="filled" 
                 size="small" 
                 multiline
                 rows={4}
                 slotProps={textFieldSlotProps}
-                onChange={handleChange}
-                onBlur={handleChange}
-                required
-                helperText={errors?.message === FieldErrors.VALUE_MISSING ? t('form-mandatory', {field: t('contact-message')}) : undefined}
+                helperText={typeof errors?.message?.message === 'string' && errors?.message?.message}
             />
             <Button 
                 variant="contained" 
