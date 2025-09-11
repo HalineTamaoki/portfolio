43682b1da7430891f23379e59cad8008559de03b
Haline Tamaoki <haline.tamaoki@gmail.com>
2025-09-10 22:06:37 -0300

    Deleted empty test and configure vite to build inside build folder

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   server: {
     port: 3000
   },
+  build: {
+    outDir: 'build',
+    emptyOutDir: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
