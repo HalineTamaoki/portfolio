3a38aee065b54cb635f38100c71b65f94eb4f726
Haline Tamaoki <haline.tamaoki@gmail.com>
2026-03-23 21:16:26 -0300

    Atualização de versão e de imagem

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'portfolio',
+    })
+  ],
   server: {
     port: 3000
   },
